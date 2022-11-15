9a0b84d48b8e554e81ca697809b319fb67a3145a
MANIKANDAN0408 <115692293+MANIKANDAN0408@users.noreply.github.com>
2022-11-15 18:20:50 +0530

    Add files via upload

---
+++ B41 DAY17 TASK/script.js
@@ -0,0 +1,101 @@
+let mani = document.createElement("div");
+mani.setAttribute("class", "continer");
+
+const mani1 = document.createElement("span");
+mani1.setAttribute("id", "span");
+
+mani1.innerHTML = 10;
+
+mani.append(mani1);
+document.body.append(mani);
+
+var a = 11;
+setTimeout(() => {
+  a = a - 1;
+   span.innerHTML = a;
+     console.log(a)
+    
+
+setTimeout(() => {
+  a = a - 1;
+   span.innerHTML = a;
+     console.log(a)
+   
+
+setTimeout(() => {
+  a = a - 1;
+   span.innerHTML = a;
+     console.log(a)
+   
+
+setTimeout(() => {
+  a = a - 1;
+   span.innerHTML = a;
+     console.log(a)
+   
+
+setTimeout(() => {
+  a = a- 1;
+   span.innerHTML = a;
+    console.log(a)
+ 
+
+setTimeout(() => {
+  a = a- 1;
+   span.innerHTML = a;
+     console.log(a)
+    
+
+setTimeout(() => {
+  a = a - 1;
+   span.innerHTML = a;
+     console.log(a)
+    
+
+setTimeout(() => {
+  a = a - 1;
+   span.innerHTML = a;
+     console.log(a)
+   
+
+setTimeout(() => {
+  a = a - 1;
+   span.innerHTML = a;
+     console.log(a)
+   
+
+setTimeout(() => {
+  a = a - 1;
+   span.innerHTML = a;
+     console.log(a)
+   
+
+setTimeout(() => {
+  a = a - 1;
+   span.innerHTML = 'Happy Independence Day';
+   console.log('Happy Independence Day')
+  
+
+ 
+}, 1000);
+
+}, 1000);
+
+}, 1000);
+
+}, 1000);
+
+}, 1000);
+
+}, 1000);
+
+}, 1000);
+
+}, 1000);
+
+}, 1000);
+
+}, 1000);
+
+}, 1000);
+ 
